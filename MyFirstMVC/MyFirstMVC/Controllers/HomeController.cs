74d3c8ab3185966a96a331a4aff2bdebd5e366ce
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-23 20:12:12 -0800

    added classGetPerson

--- MyFirstMVC/MyFirstMVC/Controllers/HomeController.cs
+++ MyFirstMVC/MyFirstMVC/Controllers/HomeController.cs
@@ -8,9 +8,22 @@ namespace MyFirstMVC.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Index(string firstName, string lastName)
+        {
+            return RedirectToAction("Results", new { firstName, lastName });
+        }
+
+        public IActionResult Results(string begining, string end)
+        {
+            Search result = new Search(beginning, end);
+            return View(result);
+        }
     }
+
 }
---
+++ MyFirstMVC/MyFirstMVC/Models/TimePerson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+    }
+    public static GetPersons(string beginning, string end)
+    {
+        Beginning = beginning;
+        End = end;
+
+    }
+}
