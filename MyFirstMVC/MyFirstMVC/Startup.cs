43c3958d6ea64f55f14d0f6e1fbbdfd319922f85
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-24 12:38:58 -0800

    css link and tweaks

--- MyFirstMVC/MyFirstMVC/Startup.cs
+++ MyFirstMVC/MyFirstMVC/Startup.cs
@@ -25,6 +25,7 @@ namespace MyFirstMVC
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
