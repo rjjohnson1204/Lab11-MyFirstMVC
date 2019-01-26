5331617b0e9b9fe22e99c8ac5e7e14040ab09ab9
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-25 18:31:57 -0800

    made hotelroom and hotel class

---
+++ AsynInnLab/AsynInnLab/Models/HotelRoom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsynInnLab.Models
+{
+    public class HotelRoom
+    {
+        public int HotelID { get; set; }
+        public int RoomNumber { get; set; }
+        public decimal RoomID { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+
+
+
+        // navigation property
+        public Hotel Hotel { get; set; }
+        public Room Room { get; set; }
+    }
+
+}
